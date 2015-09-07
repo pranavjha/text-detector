49da8add883e54472916d4a09e77e5eb6b7af1b6
Pranav Shekhar Jha <prjha@ebay.com>
2015-09-07 20:19:15 +0530

    added tests

--- test/functionality.js
+++ test/functionality.js
@@ -1,4 +1,11 @@
 'use strict';
+var addon = require('../lib');
+var expect = require('chai').expect;
+
 describe('functionality', function() {
-    it('should be able to decode an image');
+    it('should be able to decode an image', function() {
+        var calculations = 100;
+        var result = addon.calculateSync(calculations);
+        expect(Math.floor(result)).to.eql(3);
+    });
 });
