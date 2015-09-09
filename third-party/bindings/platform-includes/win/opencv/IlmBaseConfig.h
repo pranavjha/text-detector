8fb7d92af8c9c59260353a591e838489097f952a
Pranav Shekhar Jha <prjha@ebay.com>
2015-09-10 01:39:26 +0530

    standardizing

---
+++ third-party/bindings/platform-includes/win/opencv/IlmBaseConfig.h
@@ -0,0 +1,15 @@
+#undef HAVE_UCONTEXT_H
+
+//
+// Define and set to 1 if the target system has support for large
+// stack sizes.
+//
+
+#undef ILMBASE_HAVE_LARGE_STACK
+
+
+//
+// Version string for runtime access
+//
+#define ILMBASE_VERSION_STRING "1.0.3"
+#define ILMBASE_PACKAGE_STRING "IlmBase 1.0.3"
